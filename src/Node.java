155d6081e3bf4e9512436d88ab119f9b6e48672b
Akhil <akhilpgvr@gmail.com>
2024-08-08 23:05:53 +0530

    added node struture

---
+++ src/Node.java
@@ -0,0 +1,12 @@
+public class Node {
+
+    int data;
+    Node leftChild;
+    Node rightChild;
+
+    Node(int data) {
+        this.data = data;
+        this.leftChild = null;
+        this.rightChild = null;
+    }
+}
